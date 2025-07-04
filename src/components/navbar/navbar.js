f631b7ad6a4d78d4aa7d99a4c99c73fee47971c0
vishalikashyap <vis48171@gmail.com>
2025-07-04 10:09:55 +0530

    mobile view responsive

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css';
 import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <header className="navbar">
             <div className="navbar-container">
-                {/* Brand Name */}
                 <span className="brand">Vishali</span>
 
+                {/* Hamburger Icon */}
+                <div className="menu-icon" onClick={toggleMenu}>
+                    {menuOpen ? <FaTimes /> : <FaBars />}
+                </div>
+
                 {/* Navigation Links */}
-                <nav className="nav-links">
-                    <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500}>Home</Link>
-                    <Link activeClass="active" to="about" spy={true} smooth={true} offset={-60} duration={500}>About</Link>
-                    <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-60} duration={500}>Skills</Link>
-                    <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-60} duration={500}>Projects</Link>
-                    <Link activeClass="active" to="resume" spy={true} smooth={true} offset={-60} duration={500}>Resume</Link>
-                    <Link activeClass="active" to="contect" spy={true} smooth={true} offset={-60} duration={500}>Contact</Link>
+                <nav className={`nav-links ${menuOpen ? 'active' : ''}`}>
+                    <Link onClick={closeMenu} to="intro" spy={true} smooth={true} offset={-100} duration={500}>Home</Link>
+                    <Link onClick={closeMenu} to="about" spy={true} smooth={true} offset={-60} duration={500}>About</Link>
+                    <Link onClick={closeMenu} to="skills" spy={true} smooth={true} offset={-60} duration={500}>Skills</Link>
+                    <Link onClick={closeMenu} to="projects" spy={true} smooth={true} offset={-60} duration={500}>Projects</Link>
+                    <Link onClick={closeMenu} to="resume" spy={true} smooth={true} offset={-60} duration={500}>Resume</Link>
+                    <Link onClick={closeMenu} to="contect" spy={true} smooth={true} offset={-60} duration={500}>Contact</Link>
                 </nav>
             </div>
         </header>
